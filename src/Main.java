abf486d321f194f49863397d90bfcaf723ecf36c
enouraam <enouraam@mail.uc.edu>
2024-09-27 13:51:23 -0400

    init commit/complete

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double spring = 25;
+        double summer = 50;
+        double fall = 100;
+        double winter = 200;
+        double total = spring + summer + fall + winter;
+        System.out.println("Your total yearly maitenece cost is "+ total);
+    }
+}
